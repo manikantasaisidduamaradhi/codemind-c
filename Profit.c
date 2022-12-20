85c83ffba551d1e41296d329267197eae4219aeb
siddu amaradhi <119720264+manikantasaisidduamaradhi@users.noreply.github.com>
2022-12-20 19:40:07 +0530

    #include<stdio.h> int main() {     int n1,n2;     scanf("%d%d",&n1,&n2);     if(n1<n2)     {         printf("Profit");     }     else     {         printf("Loss");     }     return 0; }

---
+++ Profit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    scanf("%d%d",&n1,&n2);
+    if(n1<n2)
+    {
+        printf("Profit");
+    }
+    else
+    {
+        printf("Loss");
+    }
+    return 0;
+}
